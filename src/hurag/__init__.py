e31ec2d4872caf4807394f7396b6ee4ea34a01fe
majorli <154788733@qq.com>
2026-02-18 21:54:10 +0800

    Version 0.3.2
    New Feature:
    
    - Refactored Native SSO, it supoorts simple password validation now, and
      it is implemented in a module, not inside the login function.
    
    Infrastructure Changes:
    
    - Python version changes to 3.13;
    - Dependency 'pydantic-ai' is removed, agentic RAG will be implemented
      in a new project.

--- src/hurag/__init__.py
+++ src/hurag/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "Libin, QuestYard HuRAG Team"
 __description__ = "HuRAG, A TH-GraphRAG Application From QuestYard."
 __url__ = "https://github.com/QuestYard/HuRAG"
